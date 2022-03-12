a7b38b40f6e69c0b5348663ba08e1052ad34be6a
hugosenari <hugosenari@gmail.com>
2022-03-12 04:28:55 -0300

    chore: re index nimkalc

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''An advanced parsing library for mathematical expressions and equations'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimkalc-main".url = "path:./main";
+  inputs."nimkalc-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimkalc-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimkalc-master".url = "path:./master";
+  inputs."nimkalc-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimkalc-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
